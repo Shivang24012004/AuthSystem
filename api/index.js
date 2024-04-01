3694cc4c5f819c8637fa8b09247aa515de190aec
Shivang Bhavesh Mewada <114918884+Shivang24012004@users.noreply.github.com>
2024-04-01 22:12:32 +0530

    connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import { error } from "console";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("MongoDB connected");
+}).catch((error)=>{
+    console.log(error);
+});
 
 const app=express();
 
