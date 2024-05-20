dc4686910ce46c7ff7b0faf385b514fd0ac8b392
Shivang Bhavesh Mewada <114918884+Shivang24012004@users.noreply.github.com>
2024-05-20 15:11:33 +0530

    update user details

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 import { error } from "console";
 import userRoutes from "./routes/user.route.js"
 import authRoutes from "./routes/auth.route.js"
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 const app=express();
 
 app.use(express.json());  //allow us to send json format data in res
+app.use(cookieParser());
 
 app.listen(3000,()=>{
     console.log(`Listening to port ${3000}!`);
