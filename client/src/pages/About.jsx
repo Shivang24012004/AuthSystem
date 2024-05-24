608cebe83a2398476612bc7c7344d43b17b34784
Shivang Bhavesh Mewada <114918884+Shivang24012004@users.noreply.github.com>
2024-05-24 10:40:37 +0530

    Home and About

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
-      
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-700'>About</h1>
+      <p className='mb-4 text-slate-700'>Hello everyone, this is Shivang Mewada and this is my dummy site to get knowledge of authentication system and using OAuth by FireBase</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni mollitia dolores totam velit! Voluptatibus libero vitae optio deleniti, inventore ducimus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dolor ipsam optio,In excepturi, ullam repudiandae laborum ex dolorem velit!</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni mollitia dolores totam velit! Voluptatibus libero vitae optio deleniti, inventore ducimus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dolor ipsam optio,In excepturi, ullam repudiandae laborum ex dolorem velit!</p>
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-700'>Welcome to Auth App!</h1>
+      <p className='mb-4 text-slate-700'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni mollitia dolores totam velit! Voluptatibus libero vitae optio deleniti, inventore ducimus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dolor ipsam optio,In excepturi, ullam repudiandae laborum ex dolorem velit!</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni mollitia dolores totam velit! Voluptatibus libero vitae optio deleniti, inventore ducimus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dolor ipsam optio,In excepturi, ullam repudiandae laborum ex dolorem velit!</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni mollitia dolores totam velit! Voluptatibus libero vitae optio deleniti, inventore ducimus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dolor ipsam optio,In excepturi, ullam repudiandae laborum ex dolorem velit!</p>
     </div>
   )
 }
